35da38a2806731bb3a3225574a2792e3628565f2
mdsac1441 <amdahmed0@gmail.com>
2026-07-04 16:55:49 +0530

    feat: change bscChainID type from uint16 to uint32 and set chain ID to 99110

--- contracts/System.sol
+++ contracts/System.sol
@@ -8,27 +8,47 @@ contract System {
     bool public alreadyInit;
 
     uint32 public constant CODE_OK = 0;
-    uint16 public constant bscChainID = 0x0038;
-    address public constant VALIDATOR_CONTRACT_ADDR = 0x0000000000000000000000000000000000001000;
-    address public constant SLASH_CONTRACT_ADDR = 0x0000000000000000000000000000000000001001;
-    address public constant SYSTEM_REWARD_ADDR = 0x0000000000000000000000000000000000001002;
-    address public constant LIGHT_CLIENT_ADDR = 0x0000000000000000000000000000000000001003;
-    address public constant TOKEN_HUB_ADDR = 0x0000000000000000000000000000000000001004;
-    address public constant INCENTIVIZE_ADDR = 0x0000000000000000000000000000000000001005;
-    address public constant RELAYERHUB_CONTRACT_ADDR = 0x0000000000000000000000000000000000001006;
-    address public constant GOV_HUB_ADDR = 0x0000000000000000000000000000000000001007;
-    address public constant TOKEN_MANAGER_ADDR = 0x0000000000000000000000000000000000001008;
-    address public constant CROSS_CHAIN_CONTRACT_ADDR = 0x0000000000000000000000000000000000002000;
-    address public constant STAKING_CONTRACT_ADDR = 0x0000000000000000000000000000000000002001;
-    address public constant STAKE_HUB_ADDR = 0x0000000000000000000000000000000000002002;
-    address public constant STAKE_CREDIT_ADDR = 0x0000000000000000000000000000000000002003;
-    address public constant GOVERNOR_ADDR = 0x0000000000000000000000000000000000002004;
-    address public constant GOV_TOKEN_ADDR = 0x0000000000000000000000000000000000002005;
-    address public constant TIMELOCK_ADDR = 0x0000000000000000000000000000000000002006;
-    address public constant TOKEN_RECOVER_PORTAL_ADDR = 0x0000000000000000000000000000000000003000;
+    uint32 public constant bscChainID = 0x18326;
+    address public constant VALIDATOR_CONTRACT_ADDR =
+        0x0000000000000000000000000000000000001000;
+    address public constant SLASH_CONTRACT_ADDR =
+        0x0000000000000000000000000000000000001001;
+    address public constant SYSTEM_REWARD_ADDR =
+        0x0000000000000000000000000000000000001002;
+    address public constant LIGHT_CLIENT_ADDR =
+        0x0000000000000000000000000000000000001003;
+    address public constant TOKEN_HUB_ADDR =
+        0x0000000000000000000000000000000000001004;
+    address public constant INCENTIVIZE_ADDR =
+        0x0000000000000000000000000000000000001005;
+    address public constant RELAYERHUB_CONTRACT_ADDR =
+        0x0000000000000000000000000000000000001006;
+    address public constant GOV_HUB_ADDR =
+        0x0000000000000000000000000000000000001007;
+    address public constant TOKEN_MANAGER_ADDR =
+        0x0000000000000000000000000000000000001008;
+    address public constant CROSS_CHAIN_CONTRACT_ADDR =
+        0x0000000000000000000000000000000000002000;
+    address public constant STAKING_CONTRACT_ADDR =
+        0x0000000000000000000000000000000000002001;
+    address public constant STAKE_HUB_ADDR =
+        0x0000000000000000000000000000000000002002;
+    address public constant STAKE_CREDIT_ADDR =
+        0x0000000000000000000000000000000000002003;
+    address public constant GOVERNOR_ADDR =
+        0x0000000000000000000000000000000000002004;
+    address public constant GOV_TOKEN_ADDR =
+        0x0000000000000000000000000000000000002005;
+    address public constant TIMELOCK_ADDR =
+        0x0000000000000000000000000000000000002006;
+    address public constant TOKEN_RECOVER_PORTAL_ADDR =
+        0x0000000000000000000000000000000000003000;
 
     modifier onlyCoinbase() {
-        require(msg.sender == block.coinbase, "the message sender must be the block producer");
+        require(
+            msg.sender == block.coinbase,
+            "the message sender must be the block producer"
+        );
         _;
     }
 
@@ -48,57 +68,90 @@ contract System {
     }
 
     modifier onlySlash() {
-        require(msg.sender == SLASH_CONTRACT_ADDR, "the message sender must be slash contract");
+        require(
+            msg.sender == SLASH_CONTRACT_ADDR,
+            "the message sender must be slash contract"
+        );
         _;
     }
 
     modifier onlyTokenHub() {
-        require(msg.sender == TOKEN_HUB_ADDR, "the message sender must be token hub contract");
+        require(
+            msg.sender == TOKEN_HUB_ADDR,
+            "the message sender must be token hub contract"
+        );
         _;
     }
 
     modifier onlyGov() {
-        require(msg.sender == GOV_HUB_ADDR, "the message sender must be governance contract");
+        require(
+            msg.sender == GOV_HUB_ADDR,
+            "the message sender must be governance contract"
+        );
         _;
     }
 
     modifier onlyValidatorContract() {
-        require(msg.sender == VALIDATOR_CONTRACT_ADDR, "the message sender must be validatorSet contract");
+        require(
+            msg.sender == VALIDATOR_CONTRACT_ADDR,
+            "the message sender must be validatorSet contract"
+        );
         _;
     }
 
     modifier onlyCrossChainContract() {
-        require(msg.sender == CROSS_CHAIN_CONTRACT_ADDR, "the message sender must be cross chain contract");
+        require(
+            msg.sender == CROSS_CHAIN_CONTRACT_ADDR,
+            "the message sender must be cross chain contract"
+        );
         _;
     }
 
     modifier onlyRelayerIncentivize() {
-        require(msg.sender == INCENTIVIZE_ADDR, "the message sender must be incentivize contract");
+        require(
+            msg.sender == INCENTIVIZE_ADDR,
+            "the message sender must be incentivize contract"
+        );
         _;
     }
 
     modifier onlyRelayer() {
-        require(IRelayerHub(RELAYERHUB_CONTRACT_ADDR).isRelayer(msg.sender), "the msg sender is not a relayer");
+        require(
+            IRelayerHub(RELAYERHUB_CONTRACT_ADDR).isRelayer(msg.sender),
+            "the msg sender is not a relayer"
+        );
         _;
     }
 
     modifier onlyTokenManager() {
-        require(msg.sender == TOKEN_MANAGER_ADDR, "the msg sender must be tokenManager");
+        require(
+            msg.sender == TOKEN_MANAGER_ADDR,
+            "the msg sender must be tokenManager"
+        );
         _;
     }
 
     modifier onlyStakeHub() {
-        require(msg.sender == STAKE_HUB_ADDR, "the msg sender must be stakeHub");
+        require(
+            msg.sender == STAKE_HUB_ADDR,
+            "the msg sender must be stakeHub"
+        );
         _;
     }
 
     modifier onlyGovernorTimelock() {
-        require(msg.sender == TIMELOCK_ADDR, "the msg sender must be governor timelock contract");
+        require(
+            msg.sender == TIMELOCK_ADDR,
+            "the msg sender must be governor timelock contract"
+        );
         _;
     }
 
     modifier onlyTokenRecoverPortal() {
-        require(msg.sender == TOKEN_RECOVER_PORTAL_ADDR, "the msg sender must be token recover portal");
+        require(
+            msg.sender == TOKEN_RECOVER_PORTAL_ADDR,
+            "the msg sender must be token recover portal"
+        );
         _;
     }
 
